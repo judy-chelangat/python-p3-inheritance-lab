561ad1de03ae0cc1650462937dd994918f515eff
judy-chelangat <judysigilai75@gmail.com>
2023-08-23 12:26:32 +0300

    completed assignement

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,25 @@ from user import User
 import random
 
 class Teacher(User):
-
+    def __init__(self, first_name, last_name, knowledge):
+        super().__init__(first_name, last_name)  # Call the parent class constructor
+        self.knowledge = knowledge
+        
     def teach(self):
-        pass
+        if self.knowledge:
+            random_index = random.randint(0, len(self.knowledge) - 1)
+            return self.knowledge[random_index]
+        else:
+            return "No knowledge available."
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
+my_teacher = Teacher("John", "Doe", knowledge)
